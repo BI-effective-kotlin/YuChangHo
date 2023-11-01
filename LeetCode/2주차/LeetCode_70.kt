68656d4b0864c80d948167922ba49b04d3ae8bbb
uch0731 <66931155+uch0731@users.noreply.github.com>
2023-11-01 21:04:42 +0900

    Update LeetCode_70.kt

--- LeetCode/2주차/LeetCode_70.kt
+++ LeetCode/2주차/LeetCode_70.kt
@@ -3,22 +3,51 @@ package secondWeek
 // leetcode 70 - https://leetcode.com/problems/climbing-stairs/description/
 fun main(args: Array<String>) {
     val n = 3
-    print(climbStairs(n))
+    print(climbStairs(n+1))
 }
 
 fun climbStairs(n: Int): Int {
-    val dp = IntArray(n)
+    if (n <= 0) return 0
 
-    if (n <= 1) {
-        return n
+    val baseMatrix = arrayOf(
+        intArrayOf(1, 1),
+        intArrayOf(1, 0),
+    )
+    val resultMatrix = matrixPower(baseMatrix, n - 1)
+
+    return resultMatrix[0][0]
+}
+
+fun matrixPower(matrix: Array<IntArray>, n: Int): Array<IntArray> {
+    if (n == 1) {
+        return matrix
     }
 
-    dp[0] = 1
-    dp[1] = 2
+    if (n % 2 == 0) {
+        val halfPower = matrixPower(matrix, n / 2)
 
-    for (i in 2..n - 1) {
-        dp[i] = dp[i - 2] + dp[i - 1]
+        return matrixMultiply(halfPower, halfPower)
+    } else {
+        val halfPower = matrixPower(matrix, (n - 1) / 2)
+        val result = matrixMultiply(halfPower, halfPower)
+
+        return matrixMultiply(result, matrix)
+    }
+}
+
+fun matrixMultiply(matrix1: Array<IntArray>, matrix2: Array<IntArray>): Array<IntArray> {
+    val result = arrayOf(
+        intArrayOf(0, 0),
+        intArrayOf(0, 0),
+    )
+
+    for (i in 0 until 2) {
+        for (j in 0 until 2) {
+            for (k in 0 until 2) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
+            }
+        }
     }
 
-    return dp[dp.size - 1]
+    return result
 }
